4519462fcd6ba37bc15e30f06993e6ee8c4a0ffa
yukiyan <wakayama0215@gmail.com>
2016-10-25 06:16:35 +0900

    :put_litter_in_its_place: Remove comment

--- src/handler.js
+++ src/handler.js
@@ -30,7 +30,4 @@ module.exports.hello = (event, context, callback) => {
       console.log(error, error.stack);
       callback(error);
     });
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
