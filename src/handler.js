fe45ef76d91ed4a750bd85854101ab0579e3d4f6
yukiyan <wakayama0215@gmail.com>
2016-10-25 01:20:57 +0900

    :sparkles: Scale instastance
    attempt = current instances * 2

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,29 @@
+const AWS = require('aws-sdk');
+const autoScaling = new AWS.AutoScaling();
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  const describeAutoScalingGroupsParams = {
+    AutoScalingGroupNames: ['yukiyan-autoscaling-group-public'],
   };
 
-  callback(null, response);
+  autoScaling.describeAutoScalingGroups(describeAutoScalingGroupsParams).promise()
+    .then((response) => {
+      console.log(JSON.stringify(response));
+      const autoScalingGroup = response.AutoScalingGroups[0];
+      const updateAutoScalingGroupParams = {
+        AutoScalingGroupName: autoScalingGroup.AutoScalingGroupName,
+        DesiredCapacity: autoScalingGroup.DesiredCapacity * 2,
+      };
+      autoScaling.updateAutoScalingGroup(updateAutoScalingGroupParams).promise();
+    })
+    .then((response) => {
+      console.log(response);
+      callback(null, response);
+    })
+    .catch((error) => {
+      console.log(error, error.stack);
+      callback(error);
+    });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
